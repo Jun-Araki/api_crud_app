33453f9236d54157f29ecf9bbec522e6c4e054a4
Jun-Araki <jun7772006@gmail.com>
2023-01-14 10:36:13 +0900

    モデル・テーブル・JSONの設定

---
+++ spec/requests/v1/posts_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "V1::Posts", type: :request do
+  describe "GET #index" do
+  end
+
+  describe "GET #show" do
+  end
+
+  describe "POST #create" do
+  end
+
+  describe "PATCH #update" do
+  end
+
+  describe "DELETE #destroy" do
+  end
+end
